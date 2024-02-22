e5c83119ec6b5a4448ca8dd44841c41f947aab31
Rabbi1089 <rabbi.ext@gmail.com>
2024-02-22 19:43:11 +0600

    Submit assignment

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const cards = document.querySelectorAll(".card");
+
+for (let index = 0; index < cards.length; index++) {
+  const card = cards[index];
+card.addEventListener('click' , function () {
+    //console.log("clicked");
+
+
+     const totalPriceElement = document.getElementById('totalPrice');
+     const totalPriceText = totalPriceElement.innerText;
+     const totalPriceNumber = parseInt(totalPriceText);
+     const totalPrice = totalPriceNumber + 550;
+     totalPriceElement.innerText = totalPrice;
+
+     if(totalPriceNumber > 4){
+        function myFunction() {
+            alert("You can select maximum 4 seat!");
+          }
+          
+     }else{
+        
+     }
+})
+}
---
+++ scripts/script1.js
@@ -0,0 +1,83 @@
+
+
+
+const cards = document.querySelectorAll(".card");
+
+for (let index = 0; index < cards.length; index++) {
+  const card = cards[index];
+  // console.log(card.innerText);
+
+  card.addEventListener('click', function () {
+    // console.log("clicked");
+    
+    // seatLeftSection
+    const seatLeftElement = document.getElementById('seatLeft');
+    const seatLeftText = seatLeftElement.innerText;
+    const seatLeftNumber = parseInt(seatLeftText);
+    // console.log(typeof seatLeftNumber);
+    
+    // Decrement left seat number
+    const newLeftSeat = seatLeftNumber - 1;
+    seatLeftElement.innerText = newLeftSeat;
+
+
+
+    // newSelectedSeat
+    const newSelectSeatElement = document.getElementById('seatSup');
+    const newSelectSeatText = newSelectSeatElement.innerText;
+    const newSelectSeatNumber = parseInt(newSelectSeatText);
+    
+    // select seat increment
+    const newSelectSeat = newSelectSeatNumber + 1;
+    newSelectSeatElement.innerText = newSelectSeat;
+
+
+    // total price
+    const totalPriceElement = document.getElementById('totalPrice');
+    const totalPriceText = totalPriceElement.innerText;
+    const totalPriceNumber = parseInt(totalPriceText);
+    
+    const totalPrice = totalPriceNumber + 550;
+    totalPriceElement.innerText = totalPrice;
+
+   
+    // pUpdate
+    // const title = document.querySelectorAll('.card').innerText;
+    // console.log(title);
+    
+    // Grand total price
+    const grandTotalElement = document.getElementById('grandTotal');
+    const grandTotalText = grandTotalElement.innerText;
+    const grandTotalNumber = parseInt(grandTotalText);
+
+    const grandTotal = grandTotalNumber + 550;
+    grandTotalElement.innerText = grandTotal;
+
+  })
+  
+}
+
+
+const btn = document.getElementById("applyBtn");
+btn.addEventListener("click", function () {
+  
+  const couponElement = document.getElementById("couponInput").value;
+  // console.log(couponElement);
+
+  if (couponElement === "NEW15") {
+    // Discount calculation
+    const discountElement = document.getElementById("discountPrice");
+    const discountAmount = totalPrice * 0.15;
+    discountElement.innerText = discountAmount.toFixed(2);
+    
+    
+
+    
+
+
+
+
+  }
+
+
+})
